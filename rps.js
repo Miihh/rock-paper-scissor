ca2c1f3c1a677020d40f779e4aa4d49e516bbfc7
Mihai Blăgniceanu <m.blagniceanu@gmail.com>
2022-09-05 18:28:00 +0300

    first commit on rps game

---
+++ rps.js
@@ -0,0 +1,74 @@
+// rock, paper, scissors
+
+let hands = ["rock", "paper", "scissor"];
+let player1 = document.getElementById("rps-player1");
+let player2 = document.getElementById("rps-player2");
+
+function getHand() {
+  let randomIndex = Math.floor(Math.random() * 3); //trebuie sa cream un index, randomIndex
+  //  care va fi egal cu un numar, Math.random, dar numarul trebuie rotunjit fara zecinale, deci Math.floor
+  // apoi, folosim numarul generat de randomIndex ca index al lui hands, pt ca va asocia nr generat cu unul dintre elementele arrayului
+  return hands[randomIndex];
+}
+
+function getHand2() {
+  let randomIndex = Math.floor(Math.random() * 3);
+  return hands[randomIndex];
+}
+
+let hand1 = getHand();
+let hand2 = getHand2();
+
+function play() {
+  player1.textContent = hand1;
+  player2.textContent = hand2;
+
+  if (player1.textContent === hands[0] && player2.textContent === hands[1]) {
+    console.log("Player 2 wins. Paper beats Rock");
+  } else if (
+    player1.textContent === hands[1] &&
+    player2.textContent === hands[0]
+  ) {
+    console.log("Player 1 wins. Paper beats Rock");
+  } else if (
+    player1.textContent === hands[0] &&
+    player2.textContent === hands[2]
+  ) {
+    console.log("Player 1 wins. Rock beats Scissor");
+  } else if (
+    player1.textContent === hands[2] &&
+    player2.textContent === hands[0]
+  ) {
+    console.log("Player 2 wins. Rock beats Scissor");
+  } else if (
+    player1.textContent === hands[1] &&
+    player2.textContent === hands[2]
+  ) {
+    console.log("Player 2 wins. Scissor beats Paper");
+  } else if (
+    player1.textContent === "paper" &&
+    player2.textContent === "scissor"
+  ) {
+    console.log("Player 1 wins. Scissor beats Paper");
+  } else {
+    console.log("It's a draw!");
+  }
+}
+// OR
+
+//   if (hand1 === hands[0] && hand2 === hands[1]) {
+//     console.log("Player 2 wins. Paper beats Rock");
+//   } else if (hand1 === hands[1] && hand2 === hands[0]) {
+//     console.log("Player 1 wins. Paper beats Rock");
+//   } else if (hand1 === hands[0] && hand2 === hands[2]) {
+//     console.log("Player 1 wins. Rock beats Scissor");
+//   } else if (hand1 === hands[2] && hand2 === hands[0]) {
+//     console.log("Player 2 wins. Rock beats Scissor");
+//   } else if (hand1 === hands[1] && hand2 === hands[2]) {
+//     console.log("Player 2 wins. Scissor beats Paper");
+//   } else if (hand1 === hands[2] && hand2 === hands[1]) {
+//     console.log("Player 1 wins. Scissor beats Paper");
+//   } else {
+//     console.log("It's a draw!");
+//   }
+// }
